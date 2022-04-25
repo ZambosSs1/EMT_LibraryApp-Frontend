934040c87eafef22e5f37b7f39bc43a4a8b83321
Bojan Simichiev <bojansimiciev@gmail.com>
2022-04-25 18:59:26 +0200

    Changing baseURL

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : 'http://localhost:5050',
+    baseURL : 'https://emt-libraryapp-backend.herokuapp.com/',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
